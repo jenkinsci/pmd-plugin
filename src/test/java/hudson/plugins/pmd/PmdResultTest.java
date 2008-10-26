17a8001d5ef2777d1cc206ae73399507207a03a9
drulli <drulli@dev.java.net>
2008-10-26 17:15:30 +0000

    Added visualization of zero-warnings records.

--- src/test/java/hudson/plugins/pmd/PmdResultTest.java
+++ src/test/java/hudson/plugins/pmd/PmdResultTest.java
@@ -1,7 +1,9 @@
 package hudson.plugins.pmd;
 
+import static junit.framework.Assert.*;
 import hudson.model.AbstractBuild;
 import hudson.plugins.pmd.util.AbstractAnnotationsBuildResultTest;
+import hudson.plugins.pmd.util.AnnotationsBuildResult;
 import hudson.plugins.pmd.util.ParserResult;
 
 /**
@@ -18,6 +20,32 @@ public class PmdResultTest extends AbstractAnnotationsBuildResultTest<PmdResult>
     @Override
     protected PmdResult createBuildResult(final AbstractBuild<?, ?> build, final ParserResult project, final PmdResult previous) {
         return new PmdResult(build, project, previous);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void verifyHighScoreMessage(final int expectedZeroWarningsBuildNumber, final boolean expectedIsNewHighScore, final long expectedHighScore, final int gap, final PmdResult result) {
+        if (result.hasNoAnnotations() && result.getDelta() == 0) {
+            assertTrue(result.getDetails().contains(Messages.PMD_ResultAction_NoWarningsSince(expectedZeroWarningsBuildNumber)));
+            if (expectedIsNewHighScore) {
+                long days = AnnotationsBuildResult.getDays(expectedHighScore);
+                if (days == 1) {
+                    assertTrue(result.getDetails().contains(Messages.PMD_ResultAction_OneHighScore()));
+                }
+                else {
+                    assertTrue(result.getDetails().contains(Messages.PMD_ResultAction_MultipleHighScore(days)));
+                }
+            }
+            else {
+                long days = AnnotationsBuildResult.getDays(gap);
+                if (days == 1) {
+                    assertTrue(result.getDetails().contains(Messages.PMD_ResultAction_OneNoHighScore()));
+                }
+                else {
+                    assertTrue(result.getDetails().contains(Messages.PMD_ResultAction_MultipleNoHighScore(days)));
+                }
+            }
+        }
     }
 }
 
