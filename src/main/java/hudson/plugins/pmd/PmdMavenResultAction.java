6f7ca215dde2d986edcae55122b41d5b209298ab
Ulli Hafner <ullrich.hafner@gmail.com>
2012-09-24 23:33:02 +0200

    Fixed NPE.

--- src/main/java/hudson/plugins/pmd/PmdMavenResultAction.java
+++ src/main/java/hudson/plugins/pmd/PmdMavenResultAction.java
@@ -6,8 +6,10 @@ import hudson.maven.MavenModule;
 import hudson.maven.MavenModuleSet;
 import hudson.maven.MavenModuleSetBuild;
 import hudson.model.Action;
+import hudson.model.AbstractBuild;
 import hudson.plugins.analysis.core.HealthDescriptor;
 import hudson.plugins.analysis.core.MavenResultAction;
+import hudson.plugins.analysis.core.ParserResult;
 
 import java.util.List;
 import java.util.Map;
@@ -21,23 +23,6 @@ import java.util.Map;
  */
 public class PmdMavenResultAction extends MavenResultAction<PmdResult> {
     /**
-     * Creates a new instance of {@link PmdMavenResultAction}. This instance
-     * will have no result set in the beginning. The result will be set
-     * successively after each of the modules are build.
-     *
-     * @param owner
-     *            the associated build of this action
-     * @param healthDescriptor
-     *            health descriptor to use
-     * @param defaultEncoding
-     *            the default encoding to be used when reading and parsing files
-     */
-    public PmdMavenResultAction(final MavenModuleSetBuild owner, final HealthDescriptor healthDescriptor,
-            final String defaultEncoding) {
-        super(new PmdResultAction(owner, healthDescriptor), defaultEncoding, "PMD");
-    }
-
-    /**
      * Creates a new instance of {@link PmdMavenResultAction}.
      *
      * @param owner
@@ -49,14 +34,15 @@ public class PmdMavenResultAction extends MavenResultAction<PmdResult> {
      * @param result
      *            the result in this build
      */
-    public PmdMavenResultAction(final MavenBuild owner, final HealthDescriptor healthDescriptor,
+    public PmdMavenResultAction(final AbstractBuild<?, ?> owner, final HealthDescriptor healthDescriptor,
             final String defaultEncoding, final PmdResult result) {
         super(new PmdResultAction(owner, healthDescriptor, result), defaultEncoding, "PMD");
     }
 
     /** {@inheritDoc} */
     public MavenAggregatedReport createAggregatedAction(final MavenModuleSetBuild build, final Map<MavenModule, List<MavenBuild>> moduleBuilds) {
-        return new PmdMavenResultAction(build, getHealthDescriptor(), getDisplayName());
+        return new PmdMavenResultAction(build, getHealthDescriptor(), getDisplayName(),
+                new PmdResult(build, getDefaultEncoding(), new ParserResult()));
     }
 
     /** {@inheritDoc} */
